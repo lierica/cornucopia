fed03027c7311696b39fbe749d8d7f6c278fdd08
lierica <ericaaa.li@gmail.com>
2018-02-03 13:37:49 -0500

    beginnings of auth

--- backend/app/controllers/api/v1/authentication_controller.rb
+++ backend/app/controllers/api/v1/authentication_controller.rb
@@ -1,21 +1,33 @@
 class Api::V1::AuthenticationController < ApplicationController
 
-	def create
-		user = User.find_by(email: login_params[:email])
+	  def create
+	    user = User.find_by(emailo: login_params[:email])
+	    if user && user.authenticate(login_params[:password])
+	      render json: {
+	        id: user.id,
+	        username: user.email,
+	        jwt: JWT.encode({user_id: user.id}, ENV['secret_key'], 'HS256')
+	      }
+	    else
+	      render json: {error: 'User not found'}, status: 404
+	    end
+	  end
 
-		if user && user.authenticate(login_params[:password])
-			render json: {email: user.email, id: user.id}
-		else
-			render json: {error: "User is invalid"}, status: 401
-		end
-	end
+	  def show
+	    if current_user
+	      render json: {
+	        id: current_user.id,
+	        username: current_user.username
+	      }
+	    else
+	      render json: {error: 'No id present on headers'}, status: 404
+	    end
+	  end
 
 	private
 
 	def login_params
 		params.require(:loginFormData).permit(:email, :password)
 	end
-
-
 
 end
