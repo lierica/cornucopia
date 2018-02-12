603be40eb3533b3668f08624ed82e291333e9532
lierica <ericaaa.li@gmail.com>
2018-02-12 16:47:43 -0500

    updated seed file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,3 +1,4 @@
+
 google = Organization.create(name: "Google", sector: "technology", description: "Google is a technology company that specializes in Internet-related services and products, which include online advertising technologies, search engine, cloud computing, software, and hardware.", category: "corporation")
 
 unilever = Organization.create(name: "Unilever", sector: "consumer goods", description: "Unilever is a consumer goods company that is a leading producer of cleaning agents and personal care products.", category: "corporation")
@@ -18,28 +19,24 @@ pencilsofpromise = Organization.create(name: "Pencils of Promise", sector: "educ
 
 savethechildren = Organization.create(name: "Save the Children Fund", sector: "children", description: "The Save the Children Fund is an international organization that promotes children's rights, provides relief, and helps support children in developing countries.", category:"charity")
 
+#Users
+
+erica = User.create(first_name: "Erica", last_name: "Li", role: "Head of Corporate Responsibility", email: "ericaaa.li@gmail.com", password_digest: BCrypt::Password.create("123"), phone: "7329860905", organization_id: unilever.id)
+
+justin = User.create(first_name: "Justin", last_name: "Timberlake", role: "Chief Donation Officer", email: "justin.timberlake@gmail.com", password_digest: BCrypt::Password.create("123"), phone: "1234567899", organization_id: bowerymission.id)
+
 #Surpluses
 
-surplus1 = Surplus.create(title: "Chromebooks for Education", description: "Refurbished Chromebooks looking for a new home.", category: "electronics", units: 20, unit_category: "units", available_date: "01/31/2018", organization_id: google.id, user_id: 1)
+surplus1 = Surplus.create(title: "Chromebooks for Education", description: "Refurbished Chromebooks looking for a new home.", category: "Electronics", units: 20, unit_category: "unit", available_date: "2018-01-31", claim_by_date: "2018/06/30", location: "New York", organization_id: google.id, user_id: erica.id)
 
-surplus2 = Surplus.create(title: "Toothpaste", description: "Some fresh Colgate MaxWhite off our production line.", category: "personal care", units: 5, unit_category: "boxes", available_date: "01/31/2018", organization_id: unilever.id, claimed: true, user_id: 1)
+surplus2 = Surplus.create(title: "Toothpaste", description: "Some fresh Colgate MaxWhite off our production line.", category: "Personal Care", units: 5, unit_category: "box", available_date: "2018-01-31", claim_by_date: "2018/06/30", organization_id: unilever.id, user_id: erica.id)
 
-surplus3 = Surplus.create(title: "Protein Bars", description: "Chocolate raisin protein bars. There are 20 per box.", category: "Food", units: 10, unit_category: "boxes", available_date: "01/31/2018", organization_id: nestle.id, user_id: 1)
+surplus3 = Surplus.create(title: "Protein Bars", description: "Chocolate raisin protein bars. There are 20 per box.", category: "Food", units: 10, unit_category: "box", available_date: "2018-01-31", claim_by_date: "2018/06/30", location: "New York", organization_id: nestle.id, user_id: erica.id)
 
 #Needs
 
-need1 = Need.create(title: "Snacks for the children", description: "Some of the children attend after-school programs and they are often hungry. Looking for a healthy snack to stave their hunger.", category: "Food", units: 4, unit_category: "boxes", fulfill_by_date: "02/28/2018", organization_id: savethechildren.id, user_id: 2)
+need1 = Need.create(title: "Snacks for the children", description: "Some of the children attend after-school programs and they are often hungry. Looking for a healthy snack to stave their hunger.", category: "Food", units: 4, unit_category: "box", location: "New York", fulfill_by_date: "2018/03/01", organization_id: savethechildren.id, user_id: justin.id)
 
-need2 = Need.create(title: "Breakfast foods for our residents", description: "Looking for alternatives to cereal to feed our residents.", category: "Food", units: 7, unit_category: "boxes", fulfill_by_date: "03/01/2018", organization_id: bowerymission.id, user_id: 2)
+need2 = Need.create(title: "Breakfast foods for our residents", description: "Looking for alternatives to cereal to feed our residents.", category: "Food", location: "New York", units: 7, unit_category: "box", fulfill_by_date: "2018/03/01", organization_id: bowerymission.id, user_id: justin.id)
 
-need3 = Need.create(title: "New computers for our classrooms", description: "The computers in our classrooms are in desperate need of an upgrade.", category: "electronics", units: 20, unit_category: "units", fulfill_by_date: "03/01/2018", organization_id: pencilsofpromise.id, user_id: 2)
-
-#surplusneeds
-
-surplusneed1 = SurplusNeed.create(surplus_id: 1, need_id: 1)
-
-#usersurplusneed
-
-usersurplusneed1 = UserSurplusNeed.create(user_id: 1, surplus_id: 1, need_id: 1)
-
-usersurplusneed2 = UserSurplusNeed.create(user_id: 2, surplus_id: 2, need_id: 2)
+need3 = Need.create(title: "New computers for our classrooms", description: "The computers in our classrooms are in desperate need of an upgrade.", category: "Electronics", location: "New York", units: 20, unit_category: "unit", fulfill_by_date: "2018/03/01", organization_id: pencilsofpromise.id, user_id: justin.id)
