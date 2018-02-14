2615f0f9a9b1518b107df38679e283ac67bd3f07
lierica <ericaaa.li@gmail.com>
2018-02-14 17:40:25 -0500

    changes

--- frontend/src/dashboardContent.js
+++ frontend/src/dashboardContent.js
@@ -2,15 +2,16 @@ import React from "react"
 import { connect } from "react-redux"
 import { changeView } from "./actions/loginView"
 import { changeSurplusShowId } from "./actions/surplusShowId"
+import { changeNeedShowId } from "./actions/needShowId"
 
 const DashboardContent = (props) => {
-  // let likedSurpluses = props.user.liked_surpluses.map(
-  //   (surplus) => surplus.title
-  // )
-  // let pitchedSurpluses
-
   let reviewSurpluses = props.user.surpluses.filter(
     (surplus) => surplus.pitched_needs.length > 0
+  )
+
+  let pitchedNeeds = props.surpluses.map(
+    (surplus) =>
+      surplus.pitched_needs.length > 0 ? surplus.pitchedNeeds : null
   )
 
   const handleClick = (e) => {
@@ -64,9 +65,37 @@ const DashboardContent = (props) => {
         </div>
       ) : (
         <div>
-          <h1 style={{ fontFamily: "Nunito, sans-serif" }}>
-            Needs You've Pitched
-          </h1>
+          <h2 style={{ fontFamily: "Nunito, sans-serif" }}>Open Pitches</h2>
+          <table className="ui striped table">
+            <tbody>
+              <tr>
+                <th>
+                  <h4>Your Need</h4>
+                </th>
+                <th />
+                <th>
+                  <h4>Surplus Pitched</h4>
+                </th>
+                <th />
+              </tr>
+              {reviewSurpluses.map((surplus) => {
+                return (
+                  <tr key={surplus.id}>
+                    <td>{surplus.title}</td>
+                    <td>{surplus.claim_by_date}</td>
+                    <td>{surplus.pitched_needs.length}</td>
+                    <td>
+                      <i
+                        id={surplus.id}
+                        onClick={(e) => handleClick(e)}
+                        className="angle double right large icon"
+                      />
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
         </div>
       )}
     </div>
@@ -76,10 +105,13 @@ const DashboardContent = (props) => {
 const mapStateToProps = (state) => {
   return {
     user: state.currentUser.profile,
-    userCategory: state.currentUser.profile.organization.category
+    userCategory: state.currentUser.profile.organization.category,
+    surpluses: state.surpluses
   }
 }
 
-export default connect(mapStateToProps, { changeView, changeSurplusShowId })(
-  DashboardContent
-)
+export default connect(mapStateToProps, {
+  changeView,
+  changeSurplusShowId,
+  changeNeedShowId
+})(DashboardContent)
